f6be0bb568fbcbb9aa68330cfe8459b1d4d798ba
matthewchristen <matthewchristen@northwesternmutual.com>
2022-10-03 16:59:01 +0000

    Update index.js and package-lock.json

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 
 app.get("/", (request, response) => {
-  response.send("It worked!")
+  response.send("It worked for MattC!")
 })
 
 app.listen(3000)
